6146b0b123b8221d7c51ec402cafe115b9ae814c
Kirsten Opstad <kirsten.opstad@gmail.com>
2022-10-31 14:09:32 -0700

    Add UI logic to display cities added to page
    Co-authored-by: Henry Sullivan <sullivanbhenry@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -46,4 +46,56 @@ Places.prototype.deleteDestination = function(id) {
 
 
 //UI LOGIC
+function handleSubmission(places) {
+  // Retrieve user inputs
+  const loc = document.getElementById("location").value;
+  const landmarks = []
+  // For each landmark entry, push landmark to array landmarks
+  for (i = 1; i < 4; i++) {
+    let landmark = document.getElementById("landmark" + i).value;
+    if (landmark) {
+      landmarks.push(landmark);
+    }
+  }
+  const time = document.getElementById("timeOfYear").value;
+  const notes = document.getElementById("notes").value;
+  console.log("location: " + loc)
+  console.log("landmarks: " + landmarks)
+  console.log("time of year: " + time)
+  console.log("notes: " + notes)
+  // Create new destination
+  let newDestination = new Destination(loc, landmarks, time, notes);
+  // Add destination to places
+  places.addDestination(newDestination);
+  console.log(places.destinations);
+
+  // return places;
+};
+
+function displayCity(city) {
+  const list = document.createElement("ul");
+  const listItem = document.createElement("li");
+  const body = document.querySelector("body");
+  listItem.append(city);
+  list.append(listItem);
+  body.append(list);
+}
+
+// Initialize places
+let places = new Places();
+
+window.addEventListener("load", function(){
+  console.log(places)
+  // Listen for submit
+  const form = document.querySelector("form");
+  form.addEventListener("submit", function() {
+    event.preventDefault();
+    // Retrieve user inputs
+    handleSubmission(places);
+    console.log(places)
+    let cityIndex = places.currentId
+    displayCity(places.destinations[cityIndex]["location"]);
+    // Add destination to places
+  });
+});
 // Display those properties when a user clicks on a place's name.
