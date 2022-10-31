d29f07e29e1d60a9b35d5b320ce74622f1ac1a10
Kirsten Opstad <kirsten.opstad@gmail.com>
2022-10-31 11:14:28 -0700

    Add delete destination and update destination business logic
    Co-authored-by: Henry Sullivan <sullivanbhenry@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -8,12 +8,38 @@ function Destination(location, landmarks, timeOfYear, notes) {
   this.notes = notes;
 }
 
+//Update destination properties
+function updateDestination(newLocation, newLandmarks, newTimeOfYear, newNotes) {
+  this.location = newLocation;
+  this.landmarks = newLandmarks;
+  this.timeOfYear = newTimeOfYear;
+  this.notes = newNotes;
+}
+
 // Create constructor for places
 function Places() {
   this.destinations = {};
+  this.currentId = 0
 }
 
 // Add destination to places
 Places.prototype.addDestination = function(destination){
-  this.destinations[destination.location] = destination;
+  // this.destinations[destination.location] = destination;
+  destination.id = this.addId();
+  this.destinations[destination.id] = destination
+}
+
+//Add Unique ID to each destination
+Places.prototype.addId = function() {
+  this.currentId += 1;
+  return this.currentId;
+}
+
+//Function to delete destinations
+Places.prototype.deleteDestination = function(id) {
+  if (this.destinations[id] === undefined) {
+    return false;
+  }
+  delete this.destinations[id];
+  return true;
 }
